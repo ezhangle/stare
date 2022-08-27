f662b4d125672efa32e6fdc35f6092e06f8b2c84
mulaRahul <rahulmula10@gmail.com>
2022-08-27 11:37:08 +0530

    🔢 extract led number display as widget

--- lib/widgets/digital_clock.dart
+++ lib/widgets/digital_clock.dart
@@ -4,12 +4,17 @@ import 'package:rive/rive.dart';
 import 'package:stare/widgets/tilted_pushbutton.dart';
 import 'package:stare/widgets/utils.dart';
 
-import '../data/properties.dart';
 import '../data/style.dart';
+import 'led_number_display.dart';
 
 class DigitalClock extends StatefulWidget {
+  final int hour;
+  final int minute;
+
   const DigitalClock({
     Key? key,
+    required this.hour,
+    required this.minute,
   }) : super(key: key);
 
   @override
@@ -29,47 +34,15 @@ class _DigitalClockState extends State<DigitalClock> {
   void initState() {
     super.initState();
 
+    // init hour and minute
+    hour = widget.hour;
+    minute = widget.minute;
+
     // animation controllers
     _minutePlusBtnController = OneShotAnimation("push", autoplay: false);
     _minuteMinusBtnController = OneShotAnimation("push", autoplay: false);
     _hourMinusBtnController = OneShotAnimation("push", autoplay: false);
     _hourPlusBtnController = OneShotAnimation("push", autoplay: false);
-
-    // init hour and minute
-    hour = 12;
-    minute = 1;
-  }
-
-  int? get _hourTenthPlace {
-    if (hour < 10) {
-      return null;
-    } else {
-      return (hour / 10).floor();
-    }
-  }
-
-  int get _hourOnesPlace {
-    if (hour < 10) {
-      return hour;
-    } else {
-      return hour - ((hour / 10).floor() * 10);
-    }
-  }
-
-  int get _minuteTenthPlace {
-    if (minute < 10) {
-      return 0;
-    } else {
-      return (minute / 10).floor();
-    }
-  }
-
-  int get _minuteOnesPlace {
-    if (minute < 10) {
-      return minute;
-    } else {
-      return minute - ((minute / 10).floor() * 10);
-    }
   }
 
   @override
@@ -156,13 +129,9 @@ class _DigitalClockState extends State<DigitalClock> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                _ClockLabel(number: _hourTenthPlace ?? 10),
-                const SmallRowGap(),
-                _ClockLabel(number: _hourOnesPlace),
+                DoubleLEDNumberDisplay(value: hour),
                 const _ClockSeperator(),
-                _ClockLabel(number: _minuteTenthPlace),
-                const SmallRowGap(),
-                _ClockLabel(number: _minuteOnesPlace),
+                DoubleLEDNumberDisplay(value: minute),
               ],
             ),
           ),
@@ -178,100 +147,6 @@ class _DigitalClockState extends State<DigitalClock> {
     _minutePlusBtnController.dispose();
     _minuteMinusBtnController.dispose();
     super.dispose();
-  }
-}
-
-class _ClockLabel extends StatelessWidget {
-  final int number;
-
-  const _ClockLabel({Key? key, this.number = 10}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    assert(number >= 0 && number <= 10);
-    return SizedBox(
-      height: 44,
-      width: 26,
-      child: Column(
-        children: <Widget>[
-          _HorizontalSegment(glow: clockLabels[number]![0]),
-          Row(
-            children: <Widget>[
-              _VerticalSegment(glow: clockLabels[number]![1]),
-              const Spacer(),
-              _VerticalSegment(glow: clockLabels[number]![2]),
-            ],
-          ),
-          _HorizontalSegment(glow: clockLabels[number]![3]),
-          Row(
-            children: <Widget>[
-              _VerticalSegment(glow: clockLabels[number]![4]),
-              const Spacer(),
-              _VerticalSegment(glow: clockLabels[number]![5]),
-            ],
-          ),
-          _HorizontalSegment(glow: clockLabels[number]![6]),
-        ],
-      ),
-    );
-  }
-}
-
-class _HorizontalSegment extends StatelessWidget {
-  final bool glow;
-
-  const _HorizontalSegment({Key? key, this.glow = false}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: defaultTransitionDuration,
-      width: defaultPadding,
-      height: defaultPadding / 4,
-      decoration: BoxDecoration(
-        color: glow
-            ? Theme.of(context).colorScheme.primary
-            : Theme.of(context).colorScheme.background,
-        borderRadius: BorderRadius.circular(defaultPadding / 4),
-        boxShadow: glow
-            ? <BoxShadow>[
-                BoxShadow(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  blurRadius: defaultPadding / 4,
-                ),
-              ]
-            : null,
-      ),
-    );
-  }
-}
-
-class _VerticalSegment extends StatelessWidget {
-  final bool glow;
-
-  const _VerticalSegment({Key? key, this.glow = false}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: defaultTransitionDuration,
-      width: defaultPadding / 4,
-      height: defaultPadding,
-      decoration: BoxDecoration(
-        color: glow
-            ? Theme.of(context).colorScheme.primary
-            : Theme.of(context).colorScheme.background,
-        borderRadius: BorderRadius.circular(defaultPadding / 4),
-        boxShadow: glow
-            ? <BoxShadow>[
-                BoxShadow(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  blurRadius: defaultPadding / 4,
-                ),
-              ]
-            : null,
-      ),
-    );
   }
 }
 
---
+++ lib/widgets/led_number_display.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:stare/widgets/utils.dart';
+
+import '../data/properties.dart';
+import '../data/style.dart';
+
+class DoubleLEDNumberDisplay extends StatelessWidget {
+  final int value;
+  final double segmentWidth;
+
+  const DoubleLEDNumberDisplay(
+      {Key? key, required this.value, this.segmentWidth = defaultPadding})
+      : super(key: key);
+
+  int get _tenthsPlace {
+    if (value < 10) {
+      return 10;
+    } else {
+      return (value / 10).floor();
+    }
+  }
+
+  int get _onesPlace {
+    if (value < 10) {
+      return value;
+    } else {
+      return value - (_tenthsPlace * 10);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SingleLEDNumberDisplay(value: _tenthsPlace, segmentWidth: segmentWidth),
+        const SmallRowGap(),
+        SingleLEDNumberDisplay(value: _onesPlace, segmentWidth: segmentWidth),
+      ],
+    );
+  }
+}
+
+class SingleLEDNumberDisplay extends StatelessWidget {
+  final int value;
+  final double segmentWidth;
+
+  const SingleLEDNumberDisplay(
+      {Key? key, this.value = 10, this.segmentWidth = defaultPadding})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    assert(value >= 0 && value <= 10);
+    return SizedBox(
+      height: segmentWidth * 2.75,
+      width: segmentWidth * 1.5,
+      child: Column(
+        children: <Widget>[
+          _HorizontalSegment(
+            glow: ledNumberDisplay[value]![0],
+            width: segmentWidth,
+          ),
+          Row(
+            children: <Widget>[
+              _VerticalSegment(
+                glow: ledNumberDisplay[value]![1],
+                width: segmentWidth,
+              ),
+              const Spacer(),
+              _VerticalSegment(
+                glow: ledNumberDisplay[value]![2],
+                width: segmentWidth,
+              ),
+            ],
+          ),
+          _HorizontalSegment(
+            glow: ledNumberDisplay[value]![3],
+            width: segmentWidth,
+          ),
+          Row(
+            children: <Widget>[
+              _VerticalSegment(
+                glow: ledNumberDisplay[value]![4],
+                width: segmentWidth,
+              ),
+              const Spacer(),
+              _VerticalSegment(
+                glow: ledNumberDisplay[value]![5],
+                width: segmentWidth,
+              ),
+            ],
+          ),
+          _HorizontalSegment(
+            glow: ledNumberDisplay[value]![6],
+            width: segmentWidth,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _HorizontalSegment extends StatelessWidget {
+  final bool glow;
+  final double width;
+
+  const _HorizontalSegment(
+      {Key? key, this.glow = false, this.width = defaultPadding})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: defaultTransitionDuration,
+      width: width,
+      height: width / 4,
+      decoration: BoxDecoration(
+        color: glow
+            ? Theme.of(context).colorScheme.primary
+            : Theme.of(context).colorScheme.background,
+        borderRadius: BorderRadius.circular(defaultPadding / 4),
+        boxShadow: glow
+            ? <BoxShadow>[
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  blurRadius: defaultPadding / 4,
+                ),
+              ]
+            : null,
+      ),
+    );
+  }
+}
+
+class _VerticalSegment extends StatelessWidget {
+  final bool glow;
+  final double width;
+
+  const _VerticalSegment(
+      {Key? key, this.glow = false, this.width = defaultPadding})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: defaultTransitionDuration,
+      width: width / 4,
+      height: width,
+      decoration: BoxDecoration(
+        color: glow
+            ? Theme.of(context).colorScheme.primary
+            : Theme.of(context).colorScheme.background,
+        borderRadius: BorderRadius.circular(defaultPadding / 4),
+        boxShadow: glow
+            ? <BoxShadow>[
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  blurRadius: defaultPadding / 4,
+                ),
+              ]
+            : null,
+      ),
+    );
+  }
+}
