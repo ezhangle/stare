8306c07c27b62b7f4436a8c6bbf11e0f0ff39643
mulaRahul <rahulmula10@gmail.com>
2022-09-13 17:35:47 +0530

    set `defaultPadding` to `20.0`

--- lib/data/style.dart
+++ lib/data/style.dart
@@ -7,21 +7,18 @@ const Color darkGrey = Color(0xff545454);
 const Color darkerGrey = Color(0xff1a1a1a);
 
 // * text styles
+const double buttonFontSize = 18;
 const TextStyle baseTextStyle = TextStyle(
   height: 1.25,
   fontFamily: "Poppins",
   color: Colors.black,
 );
-// final TextStyle display = baseTextStyle.copyWith(
-//   fontSize: 56,
-//   fontWeight: FontWeight.w800,
-// );
 final TextStyle headline = baseTextStyle.copyWith(
   fontSize: 44,
   fontWeight: FontWeight.w800,
 );
 final TextStyle title = baseTextStyle.copyWith(
-  fontSize: 32,
+  fontSize: 24,
   fontWeight: FontWeight.w600,
 );
 final TextStyle label = baseTextStyle.copyWith(
@@ -31,7 +28,7 @@ final TextStyle label = baseTextStyle.copyWith(
   fontWeight: FontWeight.w400,
 );
 final TextStyle body = baseTextStyle.copyWith(
-  fontSize: 20,
+  fontSize: 16,
   height: 1.5,
   color: darkGrey,
   letterSpacing: 0,
@@ -39,7 +36,7 @@ final TextStyle body = baseTextStyle.copyWith(
 );
 
 // * size and spacing
-const double defaultPadding = 16.0;
+const double defaultPadding = 20.0;
 
 // * animation
 const Duration defaultTransitionDuration = Duration(milliseconds: 400);
